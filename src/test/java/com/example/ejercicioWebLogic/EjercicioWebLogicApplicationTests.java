1dedaac0c1b90e0a77ce8297cbd7bfdfe752f8b0
FabianBahamon <bahamonsoto@gmail.com>
2026-07-24 13:42:39 -0500

    Se implementa inicio proyecto

---
+++ src/test/java/com/example/ejercicioWebLogic/EjercicioWebLogicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ejercicioWebLogic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioWebLogicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
